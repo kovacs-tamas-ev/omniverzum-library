3f8fb82011c134c111ece102561546a33950797f
kovacs-tamas-ev <kovacs.tamas.ev91@gmail.com>
2024-03-11 15:06:01 +0100

    Created connect control custom validation.

--- omniverzum-library-frontend/src/app/utils/form-utils.ts
+++ omniverzum-library-frontend/src/app/utils/form-utils.ts
@@ -1,4 +1,4 @@
-import { FormGroup } from "@angular/forms";
+import { AbstractControl, FormGroup, ValidationErrors } from "@angular/forms";
 
 export function markControlsAsTouchedAndDirty(form: FormGroup): void {
     Object.keys(form.controls).forEach(key => {
@@ -11,4 +11,19 @@ export function markControlsAsTouchedAndDirty(form: FormGroup): void {
             currentControl.markAsDirty();
         }
     })
+}
+
+export function connectControlsValidation(connectControlName: string): (control: AbstractControl) => ValidationErrors | null {
+    let statusChangeSubscription: any = null;
+    return (control: AbstractControl) => {
+        const connectControl = control.parent?.get(connectControlName);
+        if (connectControl === null || connectControl === undefined) {
+            return null;
+        }
+        if (!statusChangeSubscription) {
+            statusChangeSubscription = connectControl.statusChanges.subscribe(() => control.updateValueAndValidity({ emitEvent: false }));
+        }
+        
+        return control.value === connectControl.value ? null : { connectControls: true };
+    };
 }
