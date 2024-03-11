e463bf2e3fc4d10da7607a0dc6a50e8d4f3d4a0c
kovacs-tamas-ev <kovacs.tamas.ev91@gmail.com>
2024-03-11 20:53:03 +0100

    Added calendar localization to hungarian.

--- omniverzum-library-frontend/src/app/app.component.ts
+++ omniverzum-library-frontend/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ToastModule } from 'primeng/toast';
 import { HeaderComponent } from './header/header.component';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -13,5 +14,20 @@ import { HeaderComponent } from './header/header.component';
 })
 export class AppComponent {
   title = 'Omniverzum-könyvtár';
+
+  constructor(private primeNgConfig: PrimeNGConfig) {
+    this.initPrimeNgConfig();
+  }
+
+  private initPrimeNgConfig(): void {
+    this.primeNgConfig.setTranslation({
+      dayNamesMin: ['Va', 'Hé', 'Ke', 'Sze', 'Csü', 'Pé', 'Szo'],
+      dayNamesShort: ['Vas', 'Hét', 'Kedd', 'Szer', 'Csüt', 'Pén', 'Szom'],
+      monthNames: ['Január', 'Február', 'Március', 'Április', 'Május', 'Június', 'Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December'],
+      monthNamesShort: ['Jan', 'Feb', 'Már', 'Ápr', 'Máj', 'Jún', 'Júl', 'Aug', 'Szep', 'Okt', 'Nov', 'Dec'],
+      dateFormat: 'yy.mm.dd',
+      firstDayOfWeek: 1
+    });
+  }
 
 }
